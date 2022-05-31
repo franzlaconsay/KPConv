6ba377185e9865af25e1e85c8c833782bb9e1f29
Ervin Pelonio <ervinnino.pelonio@cit.edu>
2022-05-31 12:27:10 +0800

    small fix

--- run_single_evaluation.py
+++ run_single_evaluation.py
@@ -4,7 +4,7 @@ import shutil
 
 from datasets.Pheno4d_single import Pheno4D_Single
 parser = argparse.ArgumentParser()
-parser.add_argument('--ply_file', type=str, required=False, default='/home/ervin/Desktop/Thesis/O-CNN/tensorflow/script/dataset/shapenet_segmentation/ply_reduced_10000/Maize/M05_0325_a.ply')
+parser.add_argument('--ply_file', type=str, required=False, default='T03_0305_a.ply')
 parser.add_argument('--model', type=str, required=False, default='/home/ervin/Desktop/Thesis/O-CNN/tensorflow/script/logs/seg/pheno4d_5_k_fold_no_pre_kfold_split_3/Maize/ratio_0.01/model/iter_000020.ckpt')
 parser.add_argument('--ply2points', required=False, default='ply2points')
 
