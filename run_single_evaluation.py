d28b36c0f03efe937ce29feed35e9e26f40c1898
Ervin Pelonio <ervinnino.pelonio@cit.edu>
2022-05-31 16:27:47 +0800

    added the dataset

--- run_single_evaluation.py
+++ run_single_evaluation.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from datasets.Pheno4d_single import Pheno4D_Single
 parser = argparse.ArgumentParser()
-parser.add_argument('--ply_file', type=str, required=False, default='T03_0305_a.ply')
+parser.add_argument('--ply_file', type=str, required=False, default='Tomato/T03_0321_a.ply')
 parser.add_argument('--model', type=str, required=False, default='/home/ervin/Desktop/Thesis/O-CNN/tensorflow/script/logs/seg/pheno4d_5_k_fold_no_pre_kfold_split_3/Maize/ratio_0.01/model/iter_000020.ckpt')
 parser.add_argument('--ply2points', required=False, default='ply2points')
 
