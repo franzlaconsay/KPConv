aa6aafdeb2d58da7488c617bda416cf6643fb722
Ervin Pelonio <ervinnino.pelonio@cit.edu>
2022-05-31 12:38:19 +0800

    update run_single_evaluation.py

--- run_single_evaluation.py
+++ run_single_evaluation.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import shutil
+from utils.config import Config
 
 from datasets.Pheno4d_single import Pheno4D_Single
 parser = argparse.ArgumentParser()
@@ -15,5 +16,124 @@ if os.path.exists('evals/'):
 
 os.makedirs('evals/')
 
+def test_caller(path, step_ind, on_val):
+
+    ##########################
+    # Initiate the environment
+    ##########################
+
+    # Choose which gpu to use
+    GPU_ID = '0'
+
+    # Set GPU visible device
+    os.environ['CUDA_VISIBLE_DEVICES'] = GPU_ID
+
+    # Disable warnings
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+
+    ###########################
+    # Load the model parameters
+    ###########################
+
+    # Load model parameters
+    config = Config()
+    config.load(path)
+
+    ##################################
+    # Change model parameters for test
+    ##################################
+
+    # Change parameters for the test here. For example, you can stop augmenting the input data.
+
+    #config.augment_noise = 0.0001
+    #config.augment_color = 1.0
+    config.validation_size = 500
+    #config.batch_num = 10
+
+    ##############
+    # Prepare Data
+    ##############
+
+    print()
+    print('Dataset Preparation')
+    print('*******************')
+
+
+    dataset = Pheno4D_Single(config.dataset.split('_')[1], config.input_threads)
+
+    # Create subsample clouds of the models
+    dl0 = config.first_subsampling_dl
+    dataset.load_subsampled_clouds(dl0)
+
+    # Initialize input pipelines
+    if on_val:
+        dataset.init_input_pipeline(config)
+    else:
+        dataset.init_test_input_pipeline(config)
+
+    dataset.check_input_pipeline_training_length()
+    
+    ##############
+    # Define Model
+    ##############
+
+    print('Creating Model')
+    print('**************\n')
+    t1 = time.time()
+    model = KernelPointFCNN(dataset.flat_inputs, config)
+
+    # Find all snapshot in the chosen training folder
+    snap_path = os.path.join(path, 'snapshots')
+    snap_steps = [int(f[:-5].split('-')[-1]) for f in os.listdir(snap_path) if f[-5:] == '.meta']
+
+    # Find which snapshot to restore
+    chosen_step = np.sort(snap_steps)[step_ind]
+    chosen_snap = os.path.join(path, 'snapshots', 'snap-{:d}'.format(chosen_step))
+
+    # # Create a tester class
+    # tester = ModelTester(model, restore_snap=chosen_snap)
+    # t2 = time.time()
+
+    # print('\n----------------')
+    # print('Done in {:.1f} s'.format(t2 - t1))
+    # print('----------------\n')
+
+    # ############
+    # # Start test
+    # ############
+
+    # print('Start Test')
+    # print('**********\n')
+
+    # if config.dataset.startswith('ShapeNetPart'):
+    #     if config.dataset.split('_')[1] == 'multi':
+    #         tester.test_multi_segmentation(model, dataset)
+    #     else:
+    #         tester.test_segmentation(model, dataset)
+    # elif config.dataset.startswith('S3DIS'):
+    #     tester.test_cloud_segmentation_on_val(model, dataset)
+    # elif config.dataset.startswith('Scannet'):
+    #     if on_val:
+    #         tester.test_cloud_segmentation_on_val(model, dataset)
+    #     else:
+    #         tester.test_cloud_segmentation(model, dataset)
+    # elif config.dataset.startswith('Semantic3D'):
+    #     if on_val:
+    #         tester.test_cloud_segmentation_on_val(model, dataset)
+    #     else:
+    #         tester.test_cloud_segmentation(model, dataset)
+    # elif config.dataset.startswith('NPM3D'):
+    #     if on_val:
+    #         tester.test_cloud_segmentation_on_val(model, dataset)
+    #     else:
+    #         tester.test_cloud_segmentation(model, dataset)
+    # elif config.dataset.startswith('ModelNet40'):
+    #     tester.test_classification(model, dataset)
+        
+chosen_log = 'log/k0/'
+chosen_snapshot = -1
+on_val = False
 dataset = Pheno4D_Single('Maize', args.ply_file)
+test_caller(chosen_log, chosen_snapshot, on_val)
+
 dataset.check_input_pipeline_training_length()
